1c38625281c9264c885250cb6b67cf6dac12ae91
pagioro <pagioro@hotmail.com>
2022-09-01 20:48:56 +0000

    started adding database for the blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,125 @@
 from django.db import models
+from django.contrib.auth.models import User
+# from django.core.validators import MaxValueValidator, MinValueValidator
+# from django_summernote.fields import SummernoteTextField
+# from django.template.defaultfilters import slugify
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS_RECIPE = ((0, 'Draft'), (1, 'Published')) 
+# retirar recipe acima se der problema
+
+
+class Recipe(models.Model):
+    """
+    Model for recipe
+    """
+
+    id_recipe = models.IntegerField(unique=True)
+    title = models.CharField(max_length=200, unique=True)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+    # related_name='blog_posts'
+    slug = models.SlugField(max_length=200, unique=True)    
+    excerpt = models.TextField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)   
+    updated_date = models.DateTimeField(auto_now=True)
+    image = CloudinaryField('image', default='placeholder')
+    prep_time = models.CharField(max_length=20, unique=True)
+    cook_time = models.CharField(max_length=20, unique=True)
+    serves = models.CharField(max_length=20, unique=True)
+    calories = models.IntegerField(default=1)
+    ingredients = models.TextField()
+    # SummernoteTextField() do exemplo
+    description = models.TextField()
+    # SummernoteTextField()
+    method = models.TextField()
+    # SummernoteTextField()    
+    status_recipe = models.IntegerField(choices=STATUS_RECIPE, default=0)
+    likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
+    # do professor related_name='blog_likes'
+
+    class Meta:
+        """
+        Order the recipes in descending order
+        """
+        ordering = ['-created_date']
+
+    def __str__(self):
+        """
+        Returns a string representation of an object
+        """
+        return self.title
+
+    def number_of_likes(self):
+        """
+        Returns the number of likes on a post
+        """
+        return self.likes.count()
+
+    """
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+    """
+
+
+class Comment(models.Model):
+    """
+    Model for comment
+    """
+
+    id_comment = models.IntegerField(unique=True)
+    id_recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="comments")
+    # related_name ?
+    # id_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_comments')
+    name = models.CharField(max_length=80) 
+    # criado pelo professor
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField() 
+    # ? models.EmailField()
+    approved = models.BooleanField(default=False)
+    # status_comment = models.IntegerField(choices=STATUS_COMMENT, default=0)
+
+
+class Meta:
+    """
+    Orders the comments in ascending order
+    """
+    ordering = ['-created_date']
+
+    def __str__(self):
+        return f"Comment {self.content} by {self.name}"
+
+
+
+class Contact(models.Model):
+    """
+    Model for contact
+    """
+
+    email = models.EmailField() 
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+
+class Meta:
+    """
+    Orders the contacts in ascending order
+    """
+    ordering = ['-registration_date']
+
+"""
+class User(models.Model):
+    
+    Model for user
+    
+
+    id_user = models.IntegerField(unique=True)
+    name = models.CharField(max_length=80, unique=True)
+       
+
+class Meta:
+    
+    Orders the users in ascending order
+    
+    ordering = ['-name']
+"""
