86dbfa6fd9ab29d9c3390d290f1d7f7674529f21
pagioro <pagioro@hotmail.com>
2022-09-05 19:38:01 +0000

    Created html files and style to start the webpage

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Recipe
 
-# Create your views here.
+class RecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status_recipe=1).order_by('-created_date')
+    template_name = 'index.html'
+    paginate_by = 6
