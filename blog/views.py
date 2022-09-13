ec9a3e3e9a57b4a29b40225fecba99e9b4e64607
pagioro <pagioro@hotmail.com>
2022-09-13 19:39:18 +0000

    fixed index.html lower case recipeand middle section logic

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,4 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status_recipe=1).order_by('-created_date')
     template_name = 'index.html'
     paginate_by = 6
+    print(queryset)
