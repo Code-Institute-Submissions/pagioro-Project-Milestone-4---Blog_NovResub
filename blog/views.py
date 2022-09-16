247e15b95644aa411f1e669177c1edc2d8dec2fc
pagioro <pagioro@hotmail.com>
2022-09-16 21:56:54 +0000

    trying to solve error on post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
-from .models import Recipe
+from .models import Recipe, Comment
 
 class RecipeList(generic.ListView):
     model = Recipe
@@ -12,10 +12,11 @@ class RecipeList(generic.ListView):
 class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Recipe.objects.filter(status_recipe=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = Comment.filter(approved=True).order_by('-created_date') #voltar se estiver errado
+        post = get_object_or_404(queryset, slug=slug)        
+        comments = post.comments.filter(approved=True).order_by('-created_date') #voltar se estiver errado
         liked = False
-        if Recipe.likes.filter(id=self.request.user.id).exists(): #verificar
+        if Recipe.likes.filter(id=self.request.user).exists(): #erro 
+            # User, related_name='recipe_l
             liked = True
 
         return render(
