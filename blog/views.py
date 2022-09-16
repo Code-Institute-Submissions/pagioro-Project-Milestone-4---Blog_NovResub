a4a5f7577b6cfd9b6d08fb07c1ffc31b9c36556d
pagioro <pagioro@hotmail.com>
2022-09-16 20:29:51 +0000

    Started creating post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 class RecipeList(generic.ListView):
@@ -8,3 +8,28 @@ class RecipeList(generic.ListView):
     template_name = 'index.html'
     paginate_by = 6
     print(queryset)
+
+class PostDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status_recipe=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = Comment.filter(approved=True).order_by('-created_date') #voltar se estiver errado
+        liked = False
+        if Recipe.likes.filter(id=self.request.user.id).exists(): #verificar
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
+
+
+
+
+
+
