84d4f363c61811172b35f5ba2996d042c6e2986f
pagioro <pagioro@hotmail.com>
2022-09-17 10:14:33 +0000

    Still trying to solve code error on my post_details

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ class PostDetail(View):
         post = get_object_or_404(queryset, slug=slug)        
         comments = post.comments.filter(approved=True).order_by('-created_date') #voltar se estiver errado
         liked = False
-        if Recipe.likes.filter(id=self.request.user).exists(): #erro 
+        if post.likes.filter(id=self.request.user).exists(): #erro 
             # User, related_name='recipe_l
             liked = True
 
