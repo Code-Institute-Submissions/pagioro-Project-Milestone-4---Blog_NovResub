51a2aa6079aa338ef6bfdd1b4d18fd54a9a9929a
pagioro <pagioro@hotmail.com>
2022-09-22 10:07:26 +0000

    blog urls example to follow

--- blog/urls.py
+++ blog/urls.py
@@ -5,3 +5,29 @@ urlpatterns = [
     path("", views.RecipeList.as_view(), name="home"),
     path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
 ]
+
+
+
+# Example to follow
+
+# from django.urls import path
+# from . import views
+# from .views import RecipeDeleteView, RecipeEditView
+
+
+# urlpatterns = [
+#     path('', views.RecipeList.as_view(), name='home'),
+#     path('about/', views.about, name='about'),
+#     path('login/', views.login_page, name='login'),
+#     path('logout/', views.logout_user, name='logout'),
+#     path('signup/', views.signup_page, name='signup'),
+#     path('addrecipe/', views.add_recipe, name='addrecipe'),
+#     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
+#     path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
+#     path('recipe_edit/<slug:slug>', views.RecipeEditView.as_view(),
+#          name='recipe_edit'),
+#     path('recipe_delete/<slug:slug>', views.RecipeDeleteView.as_view(),
+#          name='recipe_delete'),
+#     path('delete_comment/<int:pk>', views.RecipeDeleteComment.as_view(),
+#          name='delete_comment'),
+# ]
