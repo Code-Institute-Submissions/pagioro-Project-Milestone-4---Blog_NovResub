9cca72a642555e5a46785d27f5eadae930488fb2
pagioro <pagioro@hotmail.com>
2022-09-22 22:00:59 +0000

    fixed liked and comment buttons

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
-from django.http import HttpResponseRedirect 
+from django.http import HttpResponseRedirect
 from .models import Recipe, Comment
 from .forms import CommentForm
 
